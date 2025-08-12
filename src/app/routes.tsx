b4c6e1f185e82d023898b94ebae33f34b6539320
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-08-12 15:29:21 -0300

    atualização package

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -9,7 +9,7 @@ import PlotManagerPage from '../modules/plot/pages/PlotManagerPage';
 import MagicSystemPage from '../modules/magic/pages/MagicSystemPage';
 import TechSystemPage from '../modules/tech/pages/TechSystemPage';
 import TimelinePage from '../modules/timeline/pages/TimelinePage';
-import RelationshipVisualizerPage from '../modules/relationships/pages/RelationshipVisualizerPage';
+import RelationshipVisualizerPage from '../modules/relationships/RelationshipVisualizerPage';
 
 const AppRoutes: React.FC = () => (
   <Routes>
