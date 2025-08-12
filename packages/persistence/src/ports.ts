524548acf954168e9c2d155e1a524a0111368a46
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-08-12 13:01:38 -0300

    reset no projeto

--- packages/persistence/src/ports.ts
+++
@@ -1,7 +0,0 @@
-// packages/persistence/src/ports.ts
-export interface Repo<T> {
-  list(): Promise<T[]>;
-  get(id: string): Promise<T|undefined>;
-  save(entity: T): Promise<void>;
-  delete(id: string): Promise<void>;
-}
