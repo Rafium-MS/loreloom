aba0cf64c213b611ca988a988799b8d03139f929
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-08-13 11:23:13 -0300

    feat: add focus mode and scene summary split

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
+import { useAppState } from '../core/state/store';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const MainLayout: React.FC<Props> = ({ children }) => (
-  <div className="flex h-screen">
-    <Sidebar />
-    <div className="flex flex-col flex-1">
-      <Topbar />
-      <main className="flex-1 overflow-auto">{children}</main>
+const MainLayout: React.FC<Props> = ({ children }) => {
+  const focusMode = useAppState((s) => s.focusMode);
+
+  return (
+    <div className="flex h-screen">
+      {!focusMode && <Sidebar />}
+      <div className="flex flex-col flex-1">
+        {!focusMode && <Topbar />}
+        <main className="flex-1 overflow-auto">{children}</main>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MainLayout;
