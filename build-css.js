3ab6669efcb08dd08c8746d027f24bdff4bd091f
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-08-22 11:50:51 -0300

    build: bundle and minify CSS

---
+++ build-css.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const entry = path.join(__dirname, 'css', 'index.css');
+const output = path.join(__dirname, 'css', 'bundle.css');
+
+function resolveImports(filePath, visited = new Set()) {
+  if (visited.has(filePath)) return '';
+  visited.add(filePath);
+  let css = fs.readFileSync(filePath, 'utf8');
+  const importRegex = /@import\s+url\(["'](.+?)["']\);?/g;
+  return css.replace(importRegex, (_match, importPath) => {
+    const resolved = path.resolve(path.dirname(filePath), importPath);
+    return resolveImports(resolved, visited);
+  });
+}
+
+function minify(css) {
+  return css
+    .replace(/\/\*[^]*?\*\//g, '')
+    .replace(/\s+/g, ' ')
+    .replace(/\s*([{}:;,])\s*/g, '$1')
+    .replace(/;}/g, '}')
+    .trim();
+}
+
+const combined = resolveImports(entry);
+const minified = minify(combined);
+fs.writeFileSync(output, minified);
+console.log(`CSS bundle written to ${output}`);
