53caa16cbe018f229e4722d3db9962fd9173b8aa
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-08-25 11:34:56 -0300

    feat: add basic electron support

---
+++ electron.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { fork } = require('child_process');
+
+let serverProcess;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+  serverProcess = fork(path.join(__dirname, 'server.js'));
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('quit', () => {
+  if (serverProcess) {
+    serverProcess.kill();
+  }
+});
